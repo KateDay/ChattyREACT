7013fd9d4b00a9430c294360c9542e8c75e77f53
KateDay <khummle@gmail.com>
2019-06-07 17:30:25 -0600

    adding styles to the messages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,31 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Message from './Message.jsx';
 
-
 class MessageList extends Component {
-    render() {
-        var messagesList = this.props.messages.map((msg) => 
-        <Message username={msg.username} content={msg.content} key={msg.id}/>
-        );
-        let notifications = this.props.messages
-            .filter((message) => message.type === 'incomingNotifaction')
-            .map((message) => message.content);
+  render() {
+    let messagesList = this.props.messages
+      .filter(message => message.type === 'incomingMessage')
+      .map(msg => (
+        <Message username={msg.username} content={msg.content} key={msg.id} />
+      ));
+    let notifications = this.props.messages
+      .filter(message => {
+        return message.type === 'incomingNotification';
+      })
+      .map(message => message.content);
 
-
-        return (
-            <main className="messages">
-                {messagesList}
-                <div className="message system">
-                    {notifications}
-                </div>
-            </main>
-        );
-    }
+    return (
+      <main className="messages">
+        {/* className="message"> */}
+        {messagesList}
+        <div className="message system">
+          {notifications}
+        </div>
+      </main>
+    );
+  }
 }
 
 MessageList.propTypes = {
-    messages: PropTypes.array
+  messages: PropTypes.array
 };
 
 export default MessageList;
