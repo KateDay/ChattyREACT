db1b425c7c6e20e2a8b45fb932a321e30d660e6f
KateDay <khummle@gmail.com>
2019-06-07 15:38:33 -0600

    adding a counter element

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Nav extends Component {
     render() {
         return (
             <nav className="navbar">
                 <a href="/" className="navbar-brand">Chatty</a>
+                <div className="userCounter">{this.props.counter}</div>
             </nav>
         );
     }
 }
+
+Nav.propTypes = {
+    counter: PropTypes.func
+};
 export default Nav;
