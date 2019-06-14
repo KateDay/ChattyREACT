5640d6cc09401b4d6f2fd95cf557f83ac0c0c2ee
Kate <khummle@gmail.com>
2019-06-14 20:57:30 +0000

    MessageList: reoganize into list items & restyle

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,31 +3,40 @@ import PropTypes from 'prop-types';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
-  render() {
-    let messagesList = this.props.messages
-      .filter(message => message.type === 'incomingMessage')
-      .map(msg => (
-        <Message username={msg.username} content={msg.content} key={msg.id} />
-      ));
+	render() {
+		let messagesList = this.props.messages
+			.filter(message => message.type === 'incomingMessage')
+			.map(msg => (
+				<li key={msg.id}>
+					<Message username={msg.username} content={msg.content} />
+				</li>
+			));
 
-    let notifications = this.props.messages
-      .filter(message => {
-        return message.type === 'incomingNotification';
-      })
-      .map((message, index) => <li key={index}>{message.content}</li>);
+		let notifications = this.props.messages
+			.filter(message => {
+				return message.type === 'incomingNotification';
+			})
+			.map((message, index) => (
+				<li className="message system" key={index}>
+					{message.content}
+				</li>
+			));
 
-    return (
-      <main className="messages">
-        {/* className="message"> */}
-        {messagesList}
-        <ul className="message system">{notifications}</ul>
-      </main>
-    );
-  }
+		return (
+			<main className="messages">
+				{/* className="message"> */}
+
+				<ul className="messageList">
+					{notifications}
+					{messagesList}
+				</ul>
+			</main>
+		);
+	}
 }
 
 MessageList.propTypes = {
-  messages: PropTypes.array
+	messages: PropTypes.array,
 };
 
 export default MessageList;
